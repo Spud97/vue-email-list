5a8ae1f659cd72c2327c1cacd67ccce8caee6a05
Spud97 <luca.dercole97@gmail.com>
2022-06-17 17:38:13 +0200

    Esercizio Svolto con Bonus

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ new Vue({
   el: "#app",
   data: {
     listaEmail: [],
-    counterAjax: 0,
   },
   methods: {
     generate10Emails() {
@@ -10,19 +9,13 @@ new Vue({
       const url = "https://flynn.boolean.careers/exercises/api/random/mail";
 
       for (let i = 0; i < 10; i++) {
-        counterAjax++;
-
         axios
           .get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then((axiosResp) => {
-            counterAjax;
-            console.log("Ricevuto risposta ciclo " + i);
             this.listaEmail.push(axiosResp.data.response);
-            console.log(listaEmail);
+            console.log(this.listaEmail);
           });
-        console.log("Eseguito ciclo " + i);
       }
-      console.log("Codice dopo la fine del ciclo");
     },
   },
 });
